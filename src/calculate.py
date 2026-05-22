c06a5f1c8d7d1723ffbe06b2d1c4894a33d8183d
hanson92828 <hanson92828@gmail.com>
2026-05-22 11:26:41 +0800

    all

--- src/calculate.py
+++ src/calculate.py
@@ -1,3 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
 def add_func(a, b):
     return a + b
 
@@ -5,3 +10,19 @@ def add_func(a, b):
 def sub_func(a, b):
     # temp = 123  # non-use var
     return a - b
+
+
+def mut_func(a: float, b: float) -> float:
+    # return "123"
+    return a * b
+
+
+@app.get("/")
+def home():
+    return {"這是簡易計算機"}
+
+
+@app.get("/add")
+def calculate_add(a: float, b: float):
+    result = add_func(a, b)
+    return {"operation:": "addition", "a": a, "b": b, "result": result}
